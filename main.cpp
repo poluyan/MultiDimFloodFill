a39dba0ff3e96113c10529ef1567ec55e142776f
Sergey Poluyan <svpoluyan@gmail.com>
2018-05-21 23:00:54 +0300

    finding start dot over created grid

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ void b4MultipleGrids(std::vector<double> init_point)
     std::vector<double> grid = {100, 100};
     std::vector<std::vector<double>> grids(grid.size());
     std::vector<double> dx(grid.size());
-    
+
     double lb = -3, ub = 3;
 
     for(size_t i = 0; i != grids.size(); i++)
@@ -38,9 +38,31 @@ void b4MultipleGrids(std::vector<double> init_point)
         grids[i] = grid;
         dx[i] = es/(num_points*2);
     }
+
+    // finding start dot over created grid
+    std::vector<int> startdot(dim);
+    for(size_t i = 0; i != startdot.size(); i++)
+    {
+        auto pos1 = std::lower_bound(grid.begin(), grid.end(), init_point[i]);
+        startdot[i] = std::distance(grid.begin(), pos1) - 1;
+    }
+
+    std::vector<std::vector<int>> points;
+    std::vector<std::vector<int>> boundary;
+
+    points.push_back(startdot);
+
+    std::set<std::vector<int>> visited;
+
+    std::vector<std::vector<double> > samples;
+
+    size_t counter = 0;
+    size_t fe_count = 0;
+
 }
 
 int main()
 {
     std::vector<double> start = {0, 0};
+
 }
